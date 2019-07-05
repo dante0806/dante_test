754e16b7c50722825470b00f9a9e5750bccf7586
develop1 <dante0806@naver.com>
2019-07-05 18:00:01 +0900

    20190705-com2

--- src/test/java/com/dante/domain/MemberRepositoryTest.java
+++ src/test/java/com/dante/domain/MemberRepositoryTest.java
@@ -1,5 +1,18 @@
 package com.dante.domain;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.dante.domain.member.MemberRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MemberRepositoryTest {
 
+	@Autowired
+	MemberRepository memberRepository;
+	
+	public void cleanup(){}
 }
